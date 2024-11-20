e4b2ff16d75bd945020b2b1e5fe0e29a52c0ceb1
tl2b2 <tl2b2hyllos@gmail.com>
2024-11-21 00:47:25 +0900

    初回コミット、やり直し

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,20 +3,33 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManagementApplication {
 
 	private String name = "Enami Kouji";
+	private String age = "37";
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/name")
-	public String getName() {
-		return name;
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "歳";
 	}
 
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age){
+		this.name = name;
+		this.age = age;
+	}
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
 }
