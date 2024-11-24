f381b307dc286b175f9b5f51ed8fba2139dd268c
tl2b2 <tl2b2hyllos@gmail.com>
2024-11-24 11:45:28 +0900

    やり直し

--- src/main/java/raisetech/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -1,8 +1,6 @@
 package raisetech.StudentManagement;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface StudentRepository {
@@ -11,4 +9,10 @@ public interface StudentRepository {
 
     @Insert("INSERT student values(#{name}, #{age})")
     void registerStudent(String name, int age);
+
+    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+    void updateStudent(String name,int age);
+
+    @Delete("DELETE FROM student WHERE name = #{name}")
+    void deleteStudent(String name);
 }
