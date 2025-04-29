5d7cad05c112a8da0a71989e12504c5f6b6ccdb7
tl2b2 <tl2b2hyllos@gmail.com>
2025-04-29 11:39:21 +0900

    第２1回課題

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -10,8 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 @Component
 public class StudentConverter {
+    /**
+     * 受講生に紐づく受講生コース情報のマッピング
+     * 受講生コース情報が複数存在するため、ループさせて組み上げる
+     * @param Students 受講生一覧
+     * @param studentsCourses　受講生コース情報のリスト
+     * @return　受講生詳細情報のリスト
+     */
     public List<StudentDetail> convertStudentDetails(List<Student> Students,
         List<StudentsCourses> studentsCourses) {
         List<StudentDetail> studentDetails = new ArrayList<>();
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -7,22 +7,43 @@ import raisetech.StudentManagement.data.Student;
 import java.util.List;
 
 /**
- * 受講生情報を扱うリポジトリ
- * 全件検索、単一条件検索、コース情報の検索が行えるクラス
+ * 受講生テーブルと受講生コース情報テーブルと紐づくRepository
+ *
  */
 @Mapper
 public interface StudentRepository {
 
 
+    /**
+     * 受講生の全件検索
+     *
+     * @return　受講生一覧（全件）
+     */
     @Select("SELECT * FROM students WHERE isDeleted = false")
     List<Student> search();
 
+    /**
+     * 受講生情報の検索
+     *
+     * @param id　受講生ID
+     * @return　受講生
+     */
     @Select("SELECT * FROM students WHERE id = #{id}")
     Student searchStudent(String id);
 
+    /**
+     * 受講生コース情報の全件検索
+     * @return　受講生コース情報（全件）
+     */
     @Select("SELECT * FROM student_couses")
     List<StudentsCourses> searchStudentsCoursesList();
 
+    /**
+     * 受講生IDに紐づく受講生コース情報の検索
+     *
+     * @param studentId　受講生ID
+     * @return　受講生IDに紐づく受講生コース情報
+     */
     @Select("SELECT * FROM student_couses WHERE student_id = #{studentId}")
     List<StudentsCourses> searchStudentsCourses(String studentId);
 
