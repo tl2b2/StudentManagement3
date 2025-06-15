d7da0523cddcb0c8a492518b36114f6e128b4f41
tl2b2 <tl2b2hyllos@gmail.com>
2025-06-15 14:21:48 +0900

    第25回課題

--- src/main/java/raisetech/StudentManagement/data/StudentCourse.java
+++ src/main/java/raisetech/StudentManagement/data/StudentCourse.java
@@ -1,9 +1,11 @@
 package raisetech.StudentManagement.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
+@Schema(description = "受講生コース情報詳細")
 @Getter
 @Setter
 public class StudentCourse {
